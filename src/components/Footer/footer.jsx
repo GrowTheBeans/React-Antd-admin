4f273b94773894ee02280370069d3d6b09623115
粽子 <rail_0039@126.com>
2020-12-19 18:21:29 +0800

    feature:项目初始化，和项目主体构建

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>Footer尾部</div>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>Header头部</div>
+    )
+  }
+}
+
+export default Header;
