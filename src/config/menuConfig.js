088891951caccdbf5fc278aafbe34944989ea8c6
粽子 <rail_0039@126.com>
2020-12-26 23:10:58 +0800

    del: 删除不需要文件

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -28,36 +28,6 @@ const menuList = [
       {
         title: '全局Message',
         key: '/ui/messages',
-<<<<<<< HEAD
-=======
-      },
-      {
-        title: 'Tab页签',
-        key: '/ui/tabs',
-      },
-      {
-        title: '图片画廊',
-        key: '/ui/gallery',
-      },
-      {
-        title: '轮播图',
-        key: '/ui/carousel',
-      }
-    ]
-  },
-  {
-    title: '表单',
-    key: '/form',
-    icon: 'form',
-    children: [
-      {
-        title: '登录',
-        key: '/form/login',
-      },
-      {
-        title: '注册',
-        key: '/form/reg',
->>>>>>> 8586e0f50f7f9182447f20a46576d05f2d728ba0
       }
     ]
   },
@@ -77,44 +47,6 @@ const menuList = [
     ]
   },
   {
-<<<<<<< HEAD
-=======
-    title: '富文本',
-    icon: 'book',
-    key: '/rich'
-  },
-  {
-    title: '城市管理',
-    icon: 'home',
-    key: '/city'
-  },
-  {
-    title: '订单管理',
-    key: '/order',
-    icon: 'money-collect',
-    btnList: [
-      {
-        title: '订单详情',
-        key: 'detail'
-      },
-      {
-        title: '结束订单',
-        key: 'finish'
-      }
-    ]
-  },
-  {
-    title: '员工管理',
-    key: '/user',
-    icon: 'contacts',
-  },
-  {
-    title: '车辆地图',
-    key: '/bikeMap',
-    icon: 'dashboard',
-  },
-  {
->>>>>>> 8586e0f50f7f9182447f20a46576d05f2d728ba0
     title: '图标',
     key: '/charts',
     icon: 'deployment-unit',
