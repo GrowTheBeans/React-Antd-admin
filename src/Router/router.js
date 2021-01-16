64241590109c0452d1e5f746969c44573f538f75
粽子 <rail_0039@126.com>
2021-01-16 22:30:49 +0800

    fix: 修复bug

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import App from '../App.js';
 import Home from '../pages/home/home.jsx';
 import Login from '../pages/login/login.jsx';
 // import Button from '../pages/AntComponents/UI/Buttons/button.jsx';
@@ -10,18 +9,18 @@ let history = createBrowserHistory();
 class IRouter extends Component {
   render() {
     return (
-      <Router className="router_containers" history={history}>
-        <App>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          {/* <Route path="*"> */}
-          {/* <Exception404 /> */}
-          {/* </Route> */}
-        </App>
+      <Router className="router_containers" history={history}>123
+        {/*<App>*/}
+        {/*  <Route exact path="/">*/}
+        {/*    <Home/>*/}
+        {/*  </Route>*/}
+        {/*  <Route path="/login">*/}
+        {/*    <Login />*/}
+        {/*  </Route>*/}
+        {/*  /!* <Route path="*"> *!/*/}
+        {/*  /!* <Exception404 /> *!/*/}
+        {/*  /!* </Route> *!/*/}
+        {/*</App>*/}
       </Router>
     )
   }
